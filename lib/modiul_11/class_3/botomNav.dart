2a2d47e7486b601822e3cd57572bc574386a10f1
Mahim123-Hassan <hmahim951@gmail.com>
2026-01-23 17:07:47 +0600

    added bottomNav

--- lib/modiul_11/class_3/botomNav.dart
+++ lib/modiul_11/class_3/botomNav.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+
+import '../class_1/gridview_builder.dart';
+import '../class_1/gridview_count.dart';
+import '../class_1/listview.dart';
+
 class Bottomnav extends StatefulWidget {
   const Bottomnav({super.key});
 
@@ -7,11 +12,27 @@ class Bottomnav extends StatefulWidget {
 }
 
 class _BottomnavState extends State<Bottomnav> {
+  int _selectedIndex = 0;
+  List page = [GridviewBuilder(), GridviewCount(), Listview()];
+  onTab(int index){
+    setState(() {
+      print("test index $index");
+      _selectedIndex=index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-
+      body: page[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: onTab,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
     );
   }
 }
